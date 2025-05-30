875c364bc9c7753726ca05b309d2c14834212d38
gabrielbntt <gabriel.fb13@puccampinas.edu.br>
2025-05-29 21:18:21 -0300

    verificacao de email adicionada

---
+++ app/src/main/java/br/edu/puc/pi3_time1/AccountActivity.kt
@@ -0,0 +1,47 @@
+package br.edu.puc.pi3_time1
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import br.edu.puc.pi3_time1.ui.theme.Pi3_time1Theme
+
+class AccountActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            Pi3_time1Theme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    Greeting(
+                        name = "Android",
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+    Text(
+        text = "Hello $name!",
+        modifier = modifier
+    )
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    Pi3_time1Theme {
+        Greeting("Android")
+    }
+}
